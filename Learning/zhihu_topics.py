7cebe82b822deff00747e8fd61933df993dc4581
duchengli <duchengli@hotmail.com>
2017-01-18 23:00:34 +0800

    20170118晚上更新

--- Learning/zhihu_topics.py
+++ Learning/zhihu_topics.py
@@ -1,28 +1,30 @@
 # -*- coding: utf-8 -*-
-__author__ = 'akiyama'
+__author__ = 'duchengli'
 import urllib
-import urllib2
+import random
+import urllib.request
 import re
 import json
-fromcom.learn.zhihuSearch.topicimport *
-fromcom.learn.zhihuSearch.contentimport *
-import cookielib
- 
-defgetTopics():
-    zhihuTopics = []
-    url = 'https://www.zhihu.com/topics'
-    cj = cookielib.CookieJar()
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-    request = urllib2.Request(url)
-    response = opener.open(request)
-    pattern = re.compile('<li.*?data-id="(.*?)"><a.*?>(.*?)</a></li>',re.S)
-    results = re.findall(pattern,response.read().decode('utf-8'))
-    for n1in results:
-        printn1[0],n1[1]
-        topic = Topic(n1[0],n1[1])
-        zhihuTopics.append(topic)
-    return zhihuTopics
- 
+
+ck={'Cookie':'all-lj=0a26bbdedef5bd9e71c728e50ba283a3; lianjia_uuid=fe53c238-0ebd-4e4a-b442-858988aae3b4; lianjia_ssid=c0c5b78c-f3a6-48ac-8c0f-798836a7cb06; _ga=GA1.2.1328057788.1471959847; CNZZDATA1253492306=1662681935-1471957055-http%253A%252F%252Fbzclk.baidu.com%252F%7C1471957055; _smt_uid=57bc5326.33a30384; CNZZDATA1254525948=797369229-1471955657-http%253A%252F%252Fbzclk.baidu.com%252F%7C1471955657; CNZZDATA1255633284=1688411013-1471956256-http%253A%252F%252Fbzclk.baidu.com%252F%7C1471956256; CNZZDATA1255604082=1834544274-1471958980-http%253A%252F%252Fbzclk.baidu.com%252F%7C1471958980; CNZZDATA1256793290=1743366477-1471956471-null%7C1471956471; miyue_hide=%20index%20%20index%20%20index%20%20index%20; select_city=110000; lianjia_token=2.0068d6f9ba11cbe982797bd08b00855b61'}
+
+hds=[{'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'},{'User-Agent':'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.12 Safari/535.11'},{'User-Agent':'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)'},{'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0'},{'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/44.0.2403.89 Chrome/44.0.2403.89 Safari/537.36'},{'User-Agent':'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50'},{'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50'},{'User-Agent':'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0'},{'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1'},{'User-Agent':'Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1'},{'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11'},{'User-Agent':'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11'},{'User-Agent':'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11'},{'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.154 Safari/537.36 LBBROWSER'}]
+
+def getTopics():
+	zhihuTopics = []
+	url = 'https://www.zhihu.com/topics'
+	ck.update(hds[random.randint(0,len(hds)-1)])
+	req=urllib.request.Request(url,headers=ck)	
+	source_code=urllib.request.urlopen(req,timeout=100).read().decode('utf-8')
+	pattern = re.compile('<li.*?data-id="(.*?)"><a.*?>(.*?)</a></li>',re.S)
+	results = re.findall(pattern,source_code)
+	print(results)
+	for n1 in results:
+		print(n1[0],n1[1])
+		#topic = Topic(n1[0],n1[1])
+		#zhihuTopics.append(topic)
+	#return zhihuTopics
+
 defgetSubTopic(topic):
     url = 'https://www.zhihu.com/node/TopicsPlazzaListV2'
     isGet = True;
@@ -32,9 +34,9 @@ defgetSubTopic(topic):
         offset = offset + 20
         values = {'method': 'next', 'params': '{"topic_id":'+topic.id+',"offset":'+str(offset)+',"hash_id":""}'}
         try:
-            data = urllib.urlencode(values)
-            request = urllib2.Request(url,data,headers)
-            response = urllib2.urlopen(request)
+            data = urllib.parse.urlencode(values)(values)
+            request = urllib.request.Request(url,data,headers)
+            response = urllib.request.urlopen(request)
             json_str = json.loads(response.read().decode('utf-8'))
             # 将获取到的数组转换成字符串
             topicMsg = '.'.join(json_str['msg'])
@@ -52,22 +54,3 @@ defgetSubTopic(topic):
     file = open(topic.name+'.txt','w')
     wiriteLog(contents,file)
     return contents
- 
-defwiriteLog(contentes,file):
-    for contentin contentes:
-        file.writelines(('\n'+content.name+'->'+content.content).encode("UTF-8"))
- 
-print '开始拉取数据...\n'
-headers = {'User-Agent' : 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:35.0) Gecko/20100101 Firefox/35.0',
-          'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
-          'X-Requested-With':'XMLHttpRequest',
-          'Referer':'https://www.zhihu.com/topics',
-          'Cookie':'__utma=51854390.517069884.1416212035.1416212035.1416212035.1; q_c1=c02bf44d00d240798bfabcfc95baeb56|1455778173000|1416205243000; _za=b1c8ae35-f986-46a2-b24a-cb9359dc6b2a; aliyungf_tc=AQAAAJ1m71jL1woArKqF22VFnL/wRy6C; _xsrf=9d494558f9271340ab24598d85b2a3c8; cap_id="MDNiMjcwM2U0MTRhNDVmYjgxZWVhOWI0NTA2OGU5OTg=|1455864276|2a4ce8247ebd3c0df5393bb5661713ad9eec01dd"; n_c=1; _alicdn_sec=56c6ba4d556557d27a0f8c876f563d12a285f33a'
-          }
-i = 0
-topics = getTopics()
-for topicin topics:
-    content = getSubTopic(topic)
-    i +=len(content)
-print '知乎总话题数为：'+str(i)
-print '拉取数据结束'
