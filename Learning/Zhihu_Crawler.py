3f842f3d143c3695d8d88806041f8f8b6fbc275f
duchengli <duchengli@hotmail.com>
2017-01-30 16:56:52 +0800

    大年初三修改

--- Learning/Zhihu_Crawler.py
+++ Learning/Zhihu_Crawler.py
@@ -5,10 +5,8 @@ import random
 import urllib.request
 import re
 import json
-
-ck={'Cookie':'q_c1=682e0963d2364cf9923d89f16556d835|1483087248000|1474607524000; d_c0="AHCAbPVwlQqPTpljSnFi-LwJwv1Mjk3jIK4=|1474607524"; _za=cfe4b82c-5980-4982-8ffb-69283db42e9d; __utma=155987696.199938811.1485053109.1485053109.1485055035.2; __utmz=155987696.1485053109.1.1.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; _zap=a15f5070-c330-47bd-9966-8e8d786e9b4f; l_cap_id="N2VhMzRmYjE1OGYxNGIyZGE0ZDVmYzU3MzdmYjQzNWY=|1483436311|bd9562715e3c22d3a7219bfe09393238ebdb57f5"; cap_id="MDBjMjZhNjk3ZGZjNDdlMmJhZmIyNDc0MjI5MDY2ODE=|1483436311|c82fb9f148c7e7dcc37e6e15e28a540faf26a20a"; r_cap_id="MTRlZGRhMDU2NTRjNGUzOWI5YzA5NmNlZGE3ZDRhZjQ=|1483436316|77c135ecc1ea4aa80a81eac36279d56fe630b355"; login="ZjVjYWQwYmQwODAxNGVmYmI0MjVmY2QxZjlkZWYzYjY=|1483436320|a87020e4a5bd8cc2a7722cc6b4a9c0501886af49"; z_c0=Mi4wQUFEQVRrUTdBQUFBY0lCczlYQ1ZDaGNBQUFCaEFsVk5JUDZTV0FEVjYyajl4a0VYMEdXYkF5Y3R5UV9VWktVZUhR|1485053180|3749455743a53566c42832d238a56bfa96e011e6; _xsrf=3c5a15d777a92df3b2be5c7fe49113ad; aliyungf_tc=AQAAALHw0Tbe4A4AshxHfXSaQd7LlMV8; __utmc=155987696; __utmv=155987696.100-1|2=registration_date=20141022=1^3=entry_date=20141022=1; __utmb=155987696.0.10.1485055035'}
-
-hds={'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6','Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'}
+import requests
+import ConfigParser
 
 def getTopics():
 	zhihuTopics=[]
@@ -35,7 +33,59 @@ def getSubTopic():
             # 将获取到的数组转换成字符串
 			#print(json_str)
 	except:
-		print('Error')
-	
-getSubTopic()
+		print('未能获取数据')
+
+		
+
+
+def create_session():
+    cf=ConfigParser.ConfigParser()
+    cf.read('config.ini')
+    cookies=cf.items('cookies')
+    cookies=dict(cookies)
+    from pprint import pprint
+    pprint(cookies)
+    email=cf.get('info','email')
+    password=cf.get('info','password')
+
+    session=requests.session()
+    login_data= {'email': email, 'password': password}
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36',
+        'Host': 'www.zhihu.com',
+        'Referer': 'http://www.zhihu.com/'
+    }
+    r = session.post('http://www.zhihu.com/login/email', data=login_data, headers=header)
+    if r.json()['r'] == 1:
+        print 'Login Failed, reason is:',
+        for m in r.json()['data']:
+            print r.json()['data'][m]
+        print 'So we use cookies to login in...'
+        has_cookies = False
+        for key in cookies:
+            if key != '__name__' and cookies[key] != '':
+                has_cookies = True
+                break
+        if has_cookies is False:
+            raise ValueError('请填写config.ini文件中的cookies项.')
+        else:
+            # r = requests.get('http://www.zhihu.com/login/email', cookies=cookies) # 实现验证码登陆
+            r = session.get('http://www.zhihu.com/login/email', cookies=cookies) # 实现验证码登陆
+
+    with open('login.html', 'w') as fp:
+        fp.write(r.content)
+
+    return session, cookies
+
+
+if __name__ == '__main__':
+    requests_session, requests_cookies = create_session()
+
+    # url = 'http://www.zhihu.com/login/email'
+    url = 'http://www.zhihu.com/topic/19552832'
+    # content = requests_session.get(url).content # 未登陆
+    # content = requests.get(url, cookies=requests_cookies).content # 已登陆
+    content = requests_session.get(url, cookies=requests_cookies).content # 已登陆
+    with open('url.html', 'w') as fp:
+        fp.write(content)
 
