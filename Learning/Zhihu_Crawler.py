ce4d3fa6dc551a2ce2bafb1b4db68d54808ad783
duchengli <duchengli@hotmail.com>
2017-02-01 22:47:10 +0800

    大年初五晚上修改

--- Learning/Zhihu_Crawler.py
+++ Learning/Zhihu_Crawler.py
@@ -1,18 +1,13 @@
-# -*- coding: utf-8 -*-
-__author__ = 'duchengli'
-import urllib
-import random
-import urllib.request
-import re
-import json
-import requests
-import ConfigParser
+import urllib.parse as up  
+import re 
+import urllib 
+from http.cookiejar import CookieJar  
 
+'''
 def getTopics():
 	zhihuTopics=[]
 	url='https://www.zhihu.com/topics'
-	ck.update(hds)
-	req=urllib.request.Request(url,headers=ck)	
+	req=urllib.request.Request(url)	
 	source_code=urllib.request.urlopen(req,timeout=100).read().decode('utf-8')
 	pattern=re.compile('<li.*?data-id="(.*?)"><a.*?>(.*?)</a></li>',re.S)
 	results=re.findall(pattern,source_code)
@@ -34,58 +29,52 @@ def getSubTopic():
 			#print(json_str)
 	except:
 		print('未能获取数据')
+'''
+def xsrf():  
+	url = 'http://www.zhihu.com'  
+	zhihu = urllib.request.urlopen(url).read().decode('utf-8')
+	pattern = re.compile(r'name="_xsrf" value="(.*?)"/>')  
+	match = pattern.findall(zhihu)  
+	xsrf = match[0]  
+	print(xsrf)
+	return xsrf  
 
-		
-
-
-def create_session():
-    cf=ConfigParser.ConfigParser()
-    cf.read('config.ini')
-    cookies=cf.items('cookies')
-    cookies=dict(cookies)
-    from pprint import pprint
-    pprint(cookies)
-    email=cf.get('info','email')
-    password=cf.get('info','password')
-
-    session=requests.session()
-    login_data= {'email': email, 'password': password}
-    header = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36',
-        'Host': 'www.zhihu.com',
-        'Referer': 'http://www.zhihu.com/'
-    }
-    r = session.post('http://www.zhihu.com/login/email', data=login_data, headers=header)
-    if r.json()['r'] == 1:
-        print 'Login Failed, reason is:',
-        for m in r.json()['data']:
-            print r.json()['data'][m]
-        print 'So we use cookies to login in...'
-        has_cookies = False
-        for key in cookies:
-            if key != '__name__' and cookies[key] != '':
-                has_cookies = True
-                break
-        if has_cookies is False:
-            raise ValueError('请填写config.ini文件中的cookies项.')
-        else:
-            # r = requests.get('http://www.zhihu.com/login/email', cookies=cookies) # 实现验证码登陆
-            r = session.get('http://www.zhihu.com/login/email', cookies=cookies) # 实现验证码登陆
-
-    with open('login.html', 'w') as fp:
-        fp.write(r.content)
-
-    return session, cookies
+'''
+def post_data():  
+	data = dict()  
+	data['_xsrf'] = xsrf  
+	data['email'] = 'duchengli@hotmail.com'  
+	data['password'] = 'dl820113'  
+	data['rememberme'] = 'y'     
+	post_data = up.urlencode(data).encode('utf-8') # 编译post数据  
+	return post_data  
+         
+hdr={'Accept':'*/*','Accept-Encoding':'gzip, deflate','Accept-Language':'zh-CN,zh;q=0.8','Connection':'keep-alive','Content-Length':'95','Content-Type':'application/x-www-form-urlencoded; charset=UTF-8','Host':'www.zhihu.com','Origin':'http://www.zhihu.com','RA-Sid':'DEADFC42-20150104-093648-9e5c2d-88ba9a','Referer':'http://www.zhihu.com/','User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36','X-Requested-With':'XMLHttpRequest'}  
+  
+#cookie上创建一个opener  
+def build_openner():  
+	cookie = CookieJar()  
+	cookie_handler = ur.HTTPCookieProcessor(cookie)  
+	opener = ur.build_opener(cookie_handler)  
+	return opener  
 
 
-if __name__ == '__main__':
-    requests_session, requests_cookies = create_session()
-
-    # url = 'http://www.zhihu.com/login/email'
-    url = 'http://www.zhihu.com/topic/19552832'
-    # content = requests_session.get(url).content # 未登陆
-    # content = requests.get(url, cookies=requests_cookies).content # 已登陆
-    content = requests_session.get(url, cookies=requests_cookies).content # 已登陆
-    with open('url.html', 'w') as fp:
-        fp.write(content)
-
+xsrf = xsrf()  
+post_data = post_data()  
+opener = build_openner()  
+ur.install_opener(opener) # 安装opener  
+  
+def main():  
+	url = 'http://www.zhihu.com/login'  
+	req = ur.Request(url, post_data, hdr)  
+	response = opener.open(req)  
+	page = response.read()  
+	#print(page)  
+	# 测试成功与否  
+	testurl = 'http://www.zhihu.com/settings/account'  
+	req = ur.urlopen(testurl)  
+	print(req.read().decode('utf-8'))  
+      
+#main()
+'''
+xsrf()
