574728f7c534a378d1e6cc152a1eb541708b81fe
duchengli <duchengli@hotmail.com>
2017-02-14 23:00:32 +0800

    继续修改

--- Quants/Hotconcept.py
+++ Quants/Hotconcept.py
@@ -3,11 +3,14 @@ import tushare as ts
 
 def ztb(code):
 	try:
-		tmps=ts.get_hist_data(code,start='2017-01-01',retry_count=10,pause=2)['p_change']
+		tmps=ts.get_hist_data(code,start='2017-01-01',retry_count=10,pause=1)['p_change']
 		return len(tmps[tmps>=9.99])
 	except:
 		print(code)
 
+csvfile=open('d:\\ztb.csv','w',newline='')
+writer=csv.writer(csvfile)
+writer.writerow([u'板块名称',u'涨停板次数'])				
 tmpdict={}
 csv_reader=csv.reader(open('c:/slist20170214.csv'))
 concept=set(row[3] for row in csv_reader)
@@ -21,3 +24,4 @@ for j in concept:
 		for k in tmp:
 			cnt=cnt+ztb(k)
 		print(j,cnt)
+		writer.writerow([j,cnt])
