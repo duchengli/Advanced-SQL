279bdc77ad632b3b039dc557335b9519a7aafc95
duchengli <duchengli@hotmail.com>
2017-02-15 17:22:40 +0800

    20170215

---
+++ Quants/Insert2DB.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+import tushare as ts
+import pymssql
+import csv
+
+engine = create_engine('mssql+pymssql://sa:dl820113@127.0.0.1:1433/TUSHARE')
+
+def insert2db(code):
+	try:
+		df=ts.get_hist_data(code,start='2015-01-01',retry_count=50,pause=1)
+		df['code']=code
+		df.to_sql('HistData',engine,if_exists='append')
+	except:
+		print(code)
+
+csv_reader=csv.reader(open('c:/industry_classified.csv'))
+slist=set(row[1] for row in csv_reader)
+
+for j in slist:
+	if j!='c_name':
+		insert2db(j)
+		print(j+'已经插入数据库')
