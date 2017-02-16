009cfd4547f05035bf7c6e2f4c4e9702a1987b8b
duchengli <duchengli@hotmail.com>
2017-02-16 16:55:41 +0800

    20170216

--- Quants/Insert2DB.py
+++ Quants/Insert2DB.py
@@ -7,16 +7,16 @@ engine = create_engine('mssql+pymssql://sa:dl820113@127.0.0.1:1433/TUSHARE')
 
 def insert2db(code):
 	try:
-		df=ts.get_hist_data(code,start='2015-01-01',retry_count=50,pause=1)
+		df=ts.get_hist_data(code,start='2017-02-01',retry_count=50,pause=1)
 		df['code']=code
 		df.to_sql('HistData',engine,if_exists='append')
 	except:
 		print(code)
 
-csv_reader=csv.reader(open('c:/industry_classified.csv'))
+csv_reader=csv.reader(open('c:/slist.csv'))
 slist=set(row[1] for row in csv_reader)
 
 for j in slist:
-	if j!='c_name':
+	if j!='code':
 		insert2db(j)
 		print(j+'已经插入数据库')
