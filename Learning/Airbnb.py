e5188e1db48fadfc97e85403ac5f0e320346f906
duchengli <duchengli@hotmail.com>
2017-09-06 17:01:56 +0800

    20170906V2

--- Learning/Airbnb.py
+++ Learning/Airbnb.py
@@ -7,13 +7,27 @@ import requests
 from bs4 import BeautifulSoup
 import lxml
 
-r = requests.get(url = 'http://esf.cdfgj.gov.cn/search?page=1')
+def get_page(pagenumber):
+    page_url = 'http://esf.cdfgj.gov.cn/search?page=%d' %pagenumber
+    r = requests.get(url = page_url)
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.text,'lxml')
+        results = soup.find_all('div', class_='pan-item clearfix')
+        for result in results:
+            #解析房屋名称
+            title = result.h2.a.text.strip()
+            #解析房屋属性
+            tmp1 = list(map(lambda x:x.string.strip() ,result.find('p',class_='p_hx').children))
+            tmp2 = [x for x in tmp1 if x != '']
+            #解析发布渠道
+            tmp3 = list(map(lambda x: x.string.strip(), result.find('p', class_='p_gx').children))
+            tmp4 = [x for x in tmp3 if x != '']
+            print(title)
+            print(tmp2)
+            print(tmp4[0])
+            print(tmp4[2])
+            print('--------------------------------------------------------------------------------')
+            #解析小区名称
 
-if r.status_code == 200:
-    soup = BeautifulSoup(r.text,'lxml')
-    result = soup.find_all('div', class_='pan-item clearfix')
 
-    print(result[0].h2.a.text.strip())
-    tmp1 = list(map(lambda x:x.string.strip() ,result[0].find('p',class_='p_hx').children))
-    #print(list(tmp))
-    print(type(tmp1))
+get_page(1)
