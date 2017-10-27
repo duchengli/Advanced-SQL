6f4ad457f55cdd5024bd48c6029884ba95611458
duchengli <duchengli@hotmail.com>
2017-10-27 17:14:29 +0800

    20171009

--- Learning/Pandas.py
+++ Learning/Pandas.py
@@ -28,5 +28,17 @@ frame3.index.name = 'year'; frame3.columns.name = 'state'
 
 obj = Series([4.5, 7.2, -5.3, 3.6], index=['d', 'b', 'a', 'c'])
 #print(obj)
-obj2 = obj.reindex(['a','b','c','d','e'])
-print(obj2)
+obj2 = obj.reindex(['a','b','c','d','e'],fill_value=0)
+obj3 = Series(['blue', 'purple', 'yellow'],index=[0,2,4])
+#
+#print(obj3.reindex([0,1,2,3,4,5],method='ffill'))
+
+frame = DataFrame(np.arange(9).reshape((3, 3)), index=['a', 'c', 'd'],columns=['Ohio', 'Texas', 'California'])
+#$print(frame)
+frame2 = frame.reindex(columns=['Texas', 'Utah', 'California'])
+#print(frame2)
+
+data = DataFrame(np.arange(16).reshape((4, 4)),index=['Ohio', 'Colorado', 'Utah', 'New York'],columns=['one', 'two', 'three', 'four'])
+#print(data)
+#print(data.drop(['one','two'],axis=1))
+print(data[:2])
