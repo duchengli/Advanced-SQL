162330edaa52331b24b81b54b22deae296f04780
duchengli <duchengli@hotmail.com>
2017-11-21 22:11:09 +0800

    20170906V3

--- Learning/Pandas2.py
+++ Learning/Pandas2.py
@@ -1,9 +1,31 @@
 import pandas as pd
 import numpy as np
 from pandas import DataFrame,Series
+import re
+import json
 
+db = json.load(open('d:/pydata-book/ch07/foods-2011-10-03.json'))
 
-frame = DataFrame(np.arange(12.).reshape((4, 3)), columns=list('bde'),index=['Utah', 'Ohio', 'Texas', 'Oregon'])
-print(frame)
-print(frame.ix[:,'d':'e'])
-print(frame.loc[:,'d':'e'])
+info_keys = ['description', 'group', 'id', 'manufacturer']
+info = DataFrame(db, columns=info_keys)
+info_keys = ['description', 'group', 'id', 'manufacturer']
+info = DataFrame(db, columns=info_keys)
+
+nutrients = []
+for rec in db:
+    fnuts = DataFrame(rec['nutrients'])
+    fnuts['id'] = rec['id']
+    nutrients.append(fnuts)
+nutrients = pd.concat(nutrients, ignore_index=True)
+nutrients = nutrients.drop_duplicates()
+
+col_mapping = {'description' : 'food','group' : 'fgroup'}
+info = info.rename(columns=col_mapping, copy=False)
+
+col_mapping = {'description' : 'nutrient','group' : 'nutgroup'}
+nutrients = nutrients.rename(columns=col_mapping, copy=False)
+
+ndata = pd.merge(nutrients, info, on='id', how='outer')
+print(ndata.head())
+print('\n')
+print(ndata.info())
