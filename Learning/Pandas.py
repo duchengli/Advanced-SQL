5770ecd5742e5e6a7a83a9d3aa5634f254533fb8
duchengli <duchengli@hotmail.com>
2017-12-29 21:37:48 +0800

    20171229更新

--- Learning/Pandas.py
+++ Learning/Pandas.py
@@ -1,10 +1,35 @@
-import pandas as pd
-import numpy as np
-from pandas import DataFrame,Series
+class Car():
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
 
-df = DataFrame({'key1' : ['a', 'a', 'b', 'b', 'a'],'key2' : ['one', 'two', 'one', 'two', 'one'],'data1' : np.random.randn(5),'data2' : np.random.randn(5)})
-print(df.groupby('key1')['data1'].mean())
-print('\n')
-print(df.groupby('key1')[['data2']].mean())
-print('\n')
-print(df.groupby('key1')[['data1','data2']].mean())
+    def get_descriptive_name(self):
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+
+    def read_odometer(self):
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery_size = 70
+
+    def describe_battery(self):
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
