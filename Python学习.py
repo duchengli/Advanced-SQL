d89336836cf0915be7e2b557fc712850d2209c7b
duchengli <duchengli@hotmail.com>
2018-04-18 16:45:15 +0800

    20180418V2

--- Python学习.py
+++ Python学习.py
@@ -1,15 +1,49 @@
 class Car():
-
-    def __init__(self, make, model, year):
-        self.make = make
+    def __init__(self, manufacturer, model, year):
+        self.manufacturer = manufacturer
         self.model = model
         self.year = year
+        self.odometer_reading = 0
 
     def get_descriptive_name(self):
-        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        long_name = str(self.year) + ' ' + self.manufacturer + ' ' + self.model
         return long_name.title()
 
-my_new_car = Car('audi', 'a4', 2016)
-print(my_new_car.get_descriptive_name())
+    def read_odometer(self):
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
 
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
 
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+class Battery():
+    def __init__(self, battery_size=60):
+        """Initialize the batteery's attributes."""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+    def get_range(self):
+        if self.battery_size == 60:
+            range = 140
+        elif self.battery_size == 85:
+            range = 185
+
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
+
+class ElectricCar(Car):
+    def __init__(self, manufacturer, model, year):
+        super().__init__(manufacturer, model, year)
+        self.battery = Battery()
+
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
