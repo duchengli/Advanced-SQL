85830b84b1a349f387a1f0cc4ea71733bf1b5d86
duchengli <duchengli@hotmail.com>
2018-04-19 16:57:20 +0800

    20180419V3

--- Python学习/alien_invasion.py
+++ Python学习/alien_invasion.py
@@ -1,24 +1,27 @@
-import sys
 import pygame
+from pygame.sprite import Group
+
 from settings import Settings
 from ship import Ship
+from alien import Alien
+import game_functions as gf
 
 def run_game():
     pygame.init()
     ai_seetings = Settings()
     screen = pygame.display.set_mode((ai_seetings.screen_width,ai_seetings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    ship = Ship(screen)
+    ship = Ship(ai_seetings,screen)
+    bullets = Group()
+#    alien = Alien(ai_seetings,screen)
+    aliens = Group()
+    gf.create_fleet(ai_seetings,screen,aliens)
 
     while True:
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        screen.fill(ai_seetings.bg_color)
-        ship.blitme()
-        pygame.display.flip()
+        gf.check_events(ai_seetings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_seetings, screen, ship, aliens, bullets)
 
 run_game()
 
