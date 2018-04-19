568ae69a761d8df12a5913b8a23e6534422ffcee
duchengli <duchengli@hotmail.com>
2018-04-19 12:35:24 +0800

    20180419V2

--- Python学习/alien_invasion.py
+++ Python学习/alien_invasion.py
@@ -1,2 +1,24 @@
 import sys
-import 
+import pygame
+from settings import Settings
+from ship import Ship
+
+def run_game():
+    pygame.init()
+    ai_seetings = Settings()
+    screen = pygame.display.set_mode((ai_seetings.screen_width,ai_seetings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    ship = Ship(screen)
+
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(ai_seetings.bg_color)
+        ship.blitme()
+        pygame.display.flip()
+
+run_game()
+
