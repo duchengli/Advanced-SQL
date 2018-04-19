07ef006eee35d89bfee256b0df3e85e2d66ab165
duchengli <duchengli@hotmail.com>
2018-04-19 22:17:25 +0800

    20180419V3

--- Python学习/settings.py
+++ Python学习/settings.py
@@ -9,5 +9,5 @@ class Settings():
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
-        self.bullets_allowed = 5
+        self.bullets_allowed = 50
 
