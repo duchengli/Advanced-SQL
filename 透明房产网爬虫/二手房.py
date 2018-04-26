e351fb1923c5376d67702d1147c102e445fbbb43
duchengli <duchengli@hotmail.com>
2018-04-26 17:10:09 +0800

    20180426V2

--- 透明房产网爬虫/二手房.py
+++ 透明房产网爬虫/二手房.py
@@ -2,17 +2,19 @@
 # -*- coding: utf-8 -*-
 # __author__ = 'Duchengli'
 # 透明房产网二手房爬虫
+# mj参数规定了面积在100-200平方内的房源
 
 import requests
 from bs4 import BeautifulSoup
 import lxml
-import pandas as pd
 import time
 import random
+import re
+import math
 
 #根据第一页上的信息条数获取翻页次数
-def count_page():
-    page_url = 'http://esf.cdfgj.gov.cn/search?page=1'
+def count_page(qy):
+    page_url = 'http://esf.cdfgj.gov.cn/search?page=1&qy=%d&mj=100,200' %qy
     retry_time = 20
     for i in range(retry_time):
         try:
@@ -25,59 +27,51 @@ def count_page():
                 break
     if r.status_code == 200:
         soup = BeautifulSoup(r.text,'lxml')
-        result = soup.find('div', class_='pan-tab clearfix')
-        print(results.text)
+        result = soup.find('div', class_='pan-tab clearfix').dt
+        total_count = int(re.sub("\D", "",result.text))
+        total_page = math.ceil(total_count/10)
+        return total_page
 
-count_page()
-# def get_page(page_number):
-#     page_url = 'http://esf.cdfgj.gov.cn/search?page=%d' %page_number
-#
-#     retry_time = 20
-#     for i in range(retry_time):
-#         try:
-#             r = requests.get(url=page_url)
-#             break
-#         except:
-#             if i < retry_time - 1:
-#                 continue
-#             else:
-#                 break
-#
-#     if r.status_code == 200:
-#         soup = BeautifulSoup(r.text,'lxml')
-#         results = soup.find_all('div', class_='pan-item clearfix')
-#         for result in results:
-#             try:
-# #                title = result.h2.a.text.strip()#解析房屋名称
-#                 tmp1 = list(map(lambda x:x.string.strip() ,result.find('p',class_='p_hx').children))#解析房屋属性
-#                 tmp2 = [x for x in tmp1 if x != '']#第二次解析房屋属性
-#
-#                 tmp3 = list(map(lambda x: x.string.strip(), result.find('p', class_='p_gx').children))
-#                 tmp4 = [x for x in tmp3 if x != '']#解析发布渠道
-#
-#                 tmp5 = result.find('p',class_='p_hx').next_sibling.next_sibling.text.strip()#解析小区名称
-#                 tmp6 = tmp5.split(' ')[0]
-#                 tmp7 = tmp5.split(' ')[1][1:].replace('[','').replace(']','')
-#                 if tmp7.find('区')!= -1:
-#                     tmp8 = tmp7[:tmp7.find('区')+1]
-#                 elif tmp7.find('县')!= -1:
-#                     tmp8 = tmp7[:tmp7.find('县')+1]
-#                 elif tmp7.find('市')!= -1:
-#                     tmp8 = tmp7[:tmp7.find('市')+1]
-#
-#                 tmp9 = result.find('strong', class_='total-price').text.strip()#解析总价
-#                 tmp10 = result.find('strong', class_='h-price').text.strip()#解析单价
-#
-#                 query = [tmp8, tmp7, tmp6, tmp2[0].replace('平米',''), tmp2[1], tmp2[2], tmp2[3].replace('年建造',''), tmp9.replace('万',''), tmp10.replace('元/平',''),tmp4[2][:11]]
-#                 esflist.append(query)
-#             except:
-#                 print(str(page_number)+'-'+'有问题数据')
-#     print('%s 页已经处理完毕' %page_number)
-#     time.sleep(random.randint(0,2))
-#
-# esflist=[]
-# for i in range(1,6754):
-#     get_page(i)
-# df = pd.DataFrame(esflist)
-# df.columns = ['区县','地址','小区','面积','户型','楼层','修建时间','总价','单价','更新时间']
-# df.to_csv('二手房列表.csv', index = False, encoding = "gbk")
+#获取页面信息
+def get_page(page_number,qy):
+    page_url = 'http://esf.cdfgj.gov.cn/search?page=%d&qy=%d&mj=100,200' %(page_number,qy)
+    retry_time = 10
+    for i in range(retry_time):
+        try:
+            r = requests.get(url=page_url)
+            break
+        except:
+            if i < retry_time - 1:
+                continue
+            else:
+                err_list.append(page_number)
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.text,'lxml')
+        results = soup.find_all('div', class_='pan-item clearfix')
+        for result in results:
+            try:
+                if result.find('div', class_='yhy').text == '已核验':#只爬取已经核验的房源
+                    id = result.find('div', class_='ynum').text.strip()#解析核验号作为唯一标志项
+                    link = 'http://esf.cdfgj.gov.cn'+result.h2.a.get('href')#解析房源连接
+                    total_price = int(result.find('strong', class_='total-price').text.strip().replace('万',''))#解析总价
+                    xqm = result.find('p',class_='p_hx').next_sibling.next_sibling.text.strip().split(' ')[0]#解析小区名称
+#                    print(xqm,id,total_price,link)
+#                    esf[id]=total_price
+                    esf_list.append((id,xqm,total_price,link))
+            except:
+                pass
+    print('%s 页已经处理完毕' %page_number)
+    time.sleep(3)
+
+esf_list = []
+esf_dict = {}
+err_page = []
+
+for i in range(1,count_page(510105)+1):
+#for i in range(1,5):
+    get_page(i,510105)
+# print(esf)
+# print(err_page)
+
+for i in sorted(esf_list, key=lambda  x:x[2], reverse=True):
+    print(i)
